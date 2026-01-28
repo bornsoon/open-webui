1ab66676a32bd1476ae3696d1a7936512b4c5d67
lhl <hyolim922@gmail.com>
2026-01-28 14:21:23 +0900

    chore: 프로젝트 개요 추가

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -654,7 +654,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Open WebUI",
     docs_url="/docs" if ENV == "dev" else None,
-    openapi_url="/openapi.json" if ENV == "dev" else None,
+    openapi_url="/openapi.json", # if ENV == "dev" else None,
     redoc_url=None,
     lifespan=lifespan,
 )
